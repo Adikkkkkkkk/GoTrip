5ca82b4a59551d0cab67b004d3f6866f218a55b9
ADIK ASUS <sfasfasf@mail.co>
2023-09-23 20:42:00 +0600

    finished the viewport

--- js/main.js
+++ js/main.js
@@ -2,9 +2,31 @@ var owl = $('.owl-carousel')
 owl.owlCarousel({
 	center: true,
 	loop: true,
-	margin: 30,
+	margin: 20,
 	startPosition: 1,
 	items: 3,
+	responsive: {
+		100: {
+			startPosition: 2,
+			margin: 10,
+			items: 2,
+		},
+		460: {
+			startPosition: 1,
+			items: 2,
+		},
+		850: {
+			startPosition: 1,
+			items: 2,
+		},
+		1000: {
+			margin: 20,
+			items: 3,
+		},
+		1270: {
+			margin: 30,
+		},
+	},
 })
 $('.slider__btn--prev').click(function () {
 	owl.trigger('prev.owl.carousel')
@@ -21,4 +43,5 @@ const nav = document.querySelector('.nav')
 document.querySelector('.menu-icon-wrapper').onclick = function () {
 	nav.classList.toggle('nav--mobile')
 	menuIcon.classList.toggle('menu-icon-active')
+	document.body.classList.toggle('no-scroll')
 }
